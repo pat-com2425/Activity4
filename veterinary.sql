3eb1694e083b5ba19bf9663dc34ec4ab2e2ab6ba
unknown <24-30496@g.batstate-u.edu.ph>
2025-11-18 21:29:11 +0800

    feat/insert-10-rows-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -46,13 +46,14 @@ CREATE TABLE invoices (
     FOREIGN KEY (appointid) REFERENCES appointments(appointid)
 );  
     
-CREATE TABLE medical_records (
+CREATE TABLE medicalrecords (
     recordid INT PRIMARY KEY,
     animalid INT,
-    visitdate DATE,
-    diagnosis VARCHAR(255),
-    treatment VARCHAR(255),
+    recorddate DATE,
     doctorid INT,
+    diagnosis VARCHAR(255),
+    prescription VARCHAR(255),
+    notes VARCHAR(255),
     FOREIGN KEY (animalid) REFERENCES animals(animalid),
     FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
 );
@@ -118,3 +119,16 @@ VALUES
 (8, 8, 150.00, '16:30:00'),
 (9, 9, 60.00, '14:45:00'),
 (10, 10, 40.00, '11:30:00');
+
+INSERT INTO medicalrecords (recordid, animalid, recorddate, doctorid, diagnosis, prescription, notes)
+VALUES 
+    (1, 1, '2023-01-05 00:00:00', 1, 'Health check', 'N/A', 'Regular checkup, no issue detected'),
+    (2, 2, '2023-01-10 00:00:00', 1, 'Vaccination', 'Vaccine X', 'Administered vaccination X as per schedule'),
+    (3, 3, '2023-02-02 00:00:00', 3, 'Sprained leg', 'Pain Medication', 'Rest recommended for two weeks'),
+    (4, 4, '2023-02-15 00:00:00', 1, 'Dental cleaning', 'N/A', 'Completed dental cleaning procedure'),
+    (5, 5, '2023-03-10 00:00:00', 4, 'Skin infection', 'Antibiotics', 'Prescribed antibiotics for skin infection'),
+    (6, 6, '2023-03-10 00:00:00', 2, 'Flea infestation', 'Flea Treatment', 'Administered flea treatment'),
+    (7, 7, '2023-04-12 00:00:00', 1, 'Vaccination', 'Vaccine Y', 'Administered vaccination Y as per schedule'),
+    (8, 8, '2023-04-18 00:00:00', 5, 'Spaying', 'N/A', 'Successfully performed spaying surgery'),
+    (9, 9, '2023-05-02 00:00:00', 4, 'Allergic reaction', 'Antihistamines', 'Allergic reaction due to food prescribed antihistamine'),
+    (10, 10, '2023-05-20 00:00:00', 6, 'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
