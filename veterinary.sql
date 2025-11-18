6397c74f808b7d4219df387025cd1ff0f5d6ae53
unknown <24-30496@g.batstate-u.edu.ph>
2025-11-18 19:17:47 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -28,3 +28,13 @@ CREATE TABLE appointments (
     reason VARCHAR(255),
     FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
+
+CREATE TABLE doctors (
+    doctorid INT PRIMARY KEY,
+    dfirstname VARCHAR(50),
+    dlastname VARCHAR(50),
+    specialization VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(50)
+);
+
